28be6a1718b0481395f783f86beb540b34f6e874
Dani-93 <min1993.dani@gmail.com>
2023-01-06 16:27:54 -0600

    No fue mucho trabjo pero fue un trabajo honesto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header(){
     return(
       
             <h1 className="font-black text-4xl text-center md:w-1/2 mx-auto text-purple-900">
-                Seguimientos Pacientes
+                Seguimientos Pacientes {""} Con Github {""}
                   <span className="text-purple-700 "> Veterinaria</span>
                 </h1>
         
